112aac114f6cd6b6e2984586aa737da748a969e8
Alpha <dlxz2022@outlook.com>
2024-09-28 20:10:53 +0800

    fix

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 sidebar_title = st.sidebar.title("Navigation")
-sidebar_slider = st.sidebar.slider("Select a value", 0, 100, 50)
-st.title("Welcome to Streamlit")
-st.write("The value you selected is:", sidebar_slider)
+sidebar_slider = st.sidebar.slider("请选择一个数值", 0, 100, 50)
+sidebar_pic=st.sidebar.image("https://pixnio.com/zh/media/zh-3162592",width=50)
+st.title("欢迎来到AlphaYin的空间")
+st.write("你导入的数值是:", sidebar_slider)
