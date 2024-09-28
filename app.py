6173943a6dae937d17103e8f5c07eac7a9d7e07e
Alpha <dlxz2022@outlook.com>
2024-09-28 20:34:55 +0800

    t

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ from PIL import Image
 image = Image.open('Ms.jpg')
 sidebar_title = st.sidebar.title("Navigation")
 sidebar_slider = st.sidebar.slider("请选择一个数值", 0, 100, 50)
-st.write("你导入的数值是:", sidebar_slider)
+st.write("孟子饭有几个女朋友", sidebar_slider)
 st.image(image, caption='孟子饭')
