53f315d3e0f9782e96f0c77ab14da77ee5af10bb
Alpha <dlxz2022@outlook.com>
2024-09-28 22:48:54 +0800

    c

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
-import streamlit as st
 import openai
-
+import streamlit as st
 with st.sidebar:
-    openai_api_key = st.text_input("OpenAI API Key",key="chatbot_api_key",type="password")
+    openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
-st.title("Chatbot")
+st.title("💬 Chatbot")
 if "messages" not in st.session_state:
-    st.session_state["messages"]=[{"role":"assistant","content":"How can I assist you?"}]
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
+
 if prompt := st.chat_input():
     if not openai_api_key:
         st.info("Please add your OpenAI API key to continue.")
