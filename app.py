21ab67a32d3e383c521f0a2e57a907399623bde4
Alpha <dlxz2022@outlook.com>
2024-09-28 20:33:49 +0800

    t

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
-image = Image.open('rock.jpg')
+image = Image.open('Ms.jpg')
 sidebar_title = st.sidebar.title("Navigation")
 sidebar_slider = st.sidebar.slider("请选择一个数值", 0, 100, 50)
 st.write("你导入的数值是:", sidebar_slider)
-st.image(image, caption='Sunrise by the mountains')
+st.image(image, caption='孟子饭')
