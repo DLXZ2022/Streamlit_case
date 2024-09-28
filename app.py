5b5cc757d61315b69bf16b60371bb8f1b76cd702
Alpha <dlxz2022@outlook.com>
2024-09-28 20:46:12 +0800

    s

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 from PIL import Image
 image = Image.open('pic1.jpg')
 image1 = Image.open('pic2.jpg')
-st.title("梦子梵大帅哥")
-st.image(image, caption='孟子饭')
-st.image(image1, caption='石头')
+st.title("作者：梦子梵大帅哥")
+st.image(image)
+st.image(image1)
