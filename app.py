ca134f57ba4e1a71a40a0ba19353055ffa9de1c2
Alpha <dlxz2022@outlook.com>
2024-09-28 20:20:07 +0800

    text2

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+from PIL import Image
+image = Image.open('rock.jpg')
 sidebar_title = st.sidebar.title("Navigation")
 sidebar_slider = st.sidebar.slider("请选择一个数值", 0, 100, 50)
-sidebar_pic=st.sidebar.image("https://pixnio.com/zh/media/zh-3162592",width=50)
-st.title("欢迎来到AlphaYin的空间")
 st.write("你导入的数值是:", sidebar_slider)
+st.image(image, caption='Sunrise by the mountains')
