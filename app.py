4617600b7b01ccd5cf7b6293f02a9ed9605470a1
Alpha <dlxz2022@outlook.com>
2024-09-28 20:44:52 +0800

    s

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
 from PIL import Image
-image = Image.open('Ms.jpg')
-image1 = Image.open('rock.jpg')
-sidebar_title = st.sidebar.title("Navigation")
-sidebar_slider = st.sidebar.slider("请选择一个数值", 0, 100, 50)
-st.write("孟子饭有",sidebar_slider,"女朋友")
+image = Image.open('pic1.jpg')
+image1 = Image.open('pic2.jpg')
+st.title("梦子梵大帅哥")
 st.image(image, caption='孟子饭')
 st.image(image1, caption='石头')
