76230405ad6198329abfbed9f33f13d3a8ba321b
Alpha <dlxz2022@outlook.com>
2024-09-30 20:00:37 +0800

    s

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ st.write('欢迎使用天命人挑选系统！')
 if st.button(':red[就决定是你了！]') :
     pf=pd.DataFrame(df,index=range(1,len(df)))
     chose=df.loc[randint(1,len(df))]
-  
-    st.write({'姓名':chose['姓名'],'学号':chose['学号'],'班级':chose['班级']})
+    st.write({'姓名':chose['姓名']})
+    tab1,tab2=st.tabs({'班级':chose['班级']}),st.tabs({'学号':chose['学号']})
